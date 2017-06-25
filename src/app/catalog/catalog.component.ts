9424d652e2452545646a6e14a8b86be7c1294f68
seanpierce <contact@seanpierce.us>
2017-06-25 07:39:45 -0700

    sort alphabetically by artist name

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -13,12 +13,19 @@ import * as firebase from 'firebase';
 })
 export class CatalogComponent implements OnInit {
   // same as in our service, set albums equal to a special firebase object array
-  albums: FirebaseListObservable<any[]>;
+  albums;
 
   constructor(private router: Router,private albumService: AlbumService) {}
 
   ngOnInit() {
-    this.albums = this.albumService.getAlbums();
+    this.albumService.getAlbums().subscribe(data => {
+      //sort albums alphabetically by artist
+      this.albums = data.sort(function(a, b) {
+        if(a.artist < b.artist) return -1;
+        if(a.artist > b.artist) return 1;
+        return 0;
+      });
+    });
   }
 
   // this method takes a clicked album (passed in) and uses it's "id" to dynamically generate (and visit) a new route
