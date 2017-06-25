91971ac72a0a3201a5114fb672499ef081d82f50
seanpierce <contact@seanpierce.us>
2017-06-24 21:22:02 -0700

    add confirmation and img preview for admin file upload

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,6 +3,8 @@ import { AlbumService } from '../album.service';
 import { Album } from '../album.model';
 import { EditAlbumComponent } from '../edit-album/edit-album.component';
 import { AuthComponent } from '../auth/auth.component';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-admin',
@@ -16,10 +18,51 @@ export class AdminComponent implements OnInit {
   audio:any;
   description:any;
 
-  constructor(private albumService: AlbumService) {
+  constructor(private albumService: AlbumService, private database: AngularFireDatabase) {
   }
 
   ngOnInit() {
+
+    // FUNCTION TO UPDATE USER DURING FILE UPLOAD PROCESS
+    var fileButton = document.getElementById('imageUpload');
+    var message = document.getElementById('progress');
+    var fileName;
+    var fileImg;
+
+    // Listen for file selection
+    fileButton.onchange = (e: any) => {
+      // get file
+      let file = e.target.files[0];
+      // create storage ref
+      var storageRef = firebase.storage().ref('uploads/' + file.name);
+      // create task to show progress
+      var task = storageRef.put(file);
+      
+      // update progress
+      task.on('state_changed',
+        async function progress(snapshot) {
+          // this will update "progress bar"
+          var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(percentage);
+          message.innerText = "uploading";
+          // when file is uploaded, set filename
+          if (percentage === 100) {
+            fileName = snapshot.metadata.name;
+            fileImg = snapshot.downloadURL;
+          }
+        },
+        function error(err) {
+          // error handling
+        },
+        async function complete() {
+          // executes when file is finished uploading
+          message.style.color = "#00b200";
+          message.innerHTML = `<img id="preview-img" src="${fileImg}"> ${fileName} - complete!`;
+          document.getElementById('preview-img').style.height = "2em";
+        }
+
+      );
+    };
   }
 
 
